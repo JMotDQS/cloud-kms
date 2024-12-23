c9722a7d112105c42ab694836e0653b40a465a4a
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-12-23 15:49:31 -0500

    Update urlRouter.js

--- js/urlRouter.js
+++ js/urlRouter.js
@@ -11,10 +11,45 @@ const cardClick = (data) => {
 	console.log("data.dataset.route:", data.dataset.route);
 }*/
 
+/*window.addEventListener('popstate', () => {
+	console.log("Navigation:window.location.pathname:", window.location.pathname);
+	var tempArr = window.location.pathname.split("/");
+	console.log(window.location.pathname.split("/"));
+	if(tempArr[2].length > 0) {
+		console.log(tempArr[2]);
+	} else {
+		tempArr[2] = "/";
+		console.log("Root directory");
+	}
+	urlLocationHandler(tempArr[2]);
+})*/
+
+const urlChangeEvent = new CustomEvent('urlChange', {
+	detail: {
+		loc: "",
+	}
+});
+window.addEventListener('urlChange', function(e) {
+	console.log("e.detail.loc:", e.detail.loc);
+	//var location = window.location.pathname;
+	if(e.detail.loc.length == 1) {
+		//location = "/cloud-kms/";
+		window.history.pushState({}, "", "/cloud-kms/");
+	} else {
+		window.history.pushState({}, "", e.detail.loc);
+	}
+	console.log("urlLocationHandler:param_location:", e.detail.loc);
+	console.log("urlLocationHandler:window.location.pathname:", window.location.pathname);
+
+	const route = urlRoutes[e.detail.loc] || urlRoutes[404];
+	//console.log("urlLocationHandler():route.page:", route.page);
+	loadPage(route.page);
+});
+
 const urlClick = (data) => {
 	//window.history.pushState({}, "", data.dataset.route);
 	urlLocationHandler(data.dataset.route);
-}
+};
 
 /*document.addEventListener("click", (e) => {
 	console.log("e:", e);
@@ -52,26 +87,30 @@ const urlRoutes = {
 		title: "Key CHeck Out",
 		description: "Get em out...Shoo Shoo Shoo"
 	}
-}
+};
 
 /*const urlRouter = (event) => {
 	event = event || window.event;
 	event.preventDefault();
 	window.history.pushState({}, "", event.target.href);
 	urlLocationHandler();
-}*/
+};*/
 
 const urlLocationHandler = async (param_location) => {
-	var location = window.location.pathname;
-	//if(location.length == 1) {
-	if(location.length == 1) {
+	/*var location = window.location.pathname;
+	if(param_location.length == 1) {
 		location = "/cloud-kms/";
+		window.history.pushState({}, "", "/cloud-kms/");
+	} else {
+		window.history.pushState({}, "", param_location);
 	}
 	console.log("urlLocationHandler:param_location:", param_location);
-	console.log("urlLocationHandler:location:", location);
-	window.history.pushState({}, "", location);
+	console.log("urlLocationHandler:window.location.pathname:", window.location.pathname);
 
 	const route = urlRoutes[param_location] || urlRoutes[404];
-	console.log("urlLocationHandler():route.page:", route.page);
-	loadPage(route.page);
-}
+	//console.log("urlLocationHandler():route.page:", route.page);
+	loadPage(route.page);*/
+
+	urlChangeEvent.detail.loc = param_location
+	window.dispatchEvent(urlChangeEvent);
+};

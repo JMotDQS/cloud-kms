946881ae2e6a8112c5443863bd453a7b8f521c00
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2025-01-09 16:53:26 -0500

    added an admin section

--- js/promises.js
+++ js/promises.js
@@ -24,6 +24,8 @@ function getSectionsPromise() {
 }
 
 function getLotsPromise(param_userid, param_admin = 0) {
+	console.log("getLotsPromise():param_userid:", param_userid);
+	console.log("getLotsPromise():param_admin:", param_admin);
 	return new Promise(function(resolve, reject) {
 		$.ajax({
 			url: "includes/getLots.php",
@@ -36,6 +38,7 @@ function getLotsPromise(param_userid, param_admin = 0) {
 			},
 
 			success: function (data) {
+				console.log("data:", data);
 				resolve(data);
 			},
 
